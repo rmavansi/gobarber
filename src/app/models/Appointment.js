24615b3239a645b9f779020a61e4f9152bde3389
Renan <rmavansi@gmail.com>
2019-12-16 12:31:22 -0300

    bugfix: cancelable_at param error

--- src/app/models/Appointment.js
+++ src/app/models/Appointment.js
@@ -16,7 +16,7 @@ class Appointment extends Model {
         cancelable: {
           type: Sequelize.VIRTUAL,
           get() {
-            return isBefore(this.date, new Date(), subHours(this.date, 2));
+            return isBefore(new Date(), subHours(this.date, 2));
           }
         }
       },
